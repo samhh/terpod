1e10f618addad9716a13e012df9e3a1608be5125
Sam A. Horvath-Hunt <hello@samhh.com>
2021-03-24 18:30:12 +0000

    Render sync progress together

--- exec/Main.hs
+++ exec/Main.hs
@@ -9,6 +9,8 @@ import           Data.List.Extra          (firstJust)
 import           Data.Map                 ()
 import qualified Data.Map                 as M
 import qualified Data.Text                as T
+import           Data.Tuple.Custom        (dup)
+import           System.Console.ANSI      (clearLine, cursorUpLine)
 import           Terpod.Cache             (CachedPodcast, findEpisode, getCache,
                                            setCache, toCached)
 import           Terpod.Config            (Config, Source (sourceUrl),
@@ -17,17 +19,6 @@ import           Terpod.Episode           (Episode (title), downloadEpisode)
 import           Terpod.Podcast           (PodcastId, getPodcast)
 import           Text.Feed.Types          (Feed)
 import           Toml                     (TomlDecodeError)
-
-renderGetPodcast :: (PodcastId, Source) -> IO (Maybe (PodcastId, Feed))
-renderGetPodcast (fid, src) = do
-  putTextLn $ ">>= Syncing " <> unpack fid <> "..."
-  res <- getPodcast . T.unpack . sourceUrl $ src
-  putTextLn $ case res of
-    Left _ -> "<#> Failed to fetch " <> unpack fid <> "!"
-    Right x -> case x of
-      Just _  -> ">>> Synced " <> unpack fid <> "."
-      Nothing -> "<#> Failed to decode " <> unpack fid <> "!"
-  pure $ (fid,) <$> fromRight Nothing res
 
 renderEpisode :: (Int, Episode) -> Text
 renderEpisode (i, ep) = "\t" <> show i <> ": " <> title ep
@@ -75,10 +66,56 @@ list opts = case podcastId opts of
       Nothing  -> putTextLn $ "Failed to find synced podcast ID: " <> unpack pid
       Just pod -> renderFeed opts pod
 
+newtype Count = Count Int deriving (Num)
+newtype Total = Total Int deriving (Num)
+newtype Failed = Failed [PodcastId] deriving (Semigroup)
+
 sync :: Config -> IO ()
 sync cfg = do
-  feeds <- catMaybes <$> mapConcurrently renderGetPodcast (M.toList $ sources cfg)
+  counter <- newIORef (Count 0)
+  failed <- newIORef (Failed [])
+
+  let pods = M.toList . sources $ cfg
+  let total = Total $ length pods
+
+  renderStart total
+  feeds <- catMaybes <$> mapConcurrently (syncOne (pushFailed failed) (renderSuccess total counter)) pods
+  renderFin total counter
+  renderFailed =<< readIORef failed
+
   setCache $ uncurry toCached <$> feeds
+
+  where renderCount :: Text -> Total -> Count -> IO ()
+        renderCount t (Total total) (Count count) =
+          putTextLn $ t <> ": " <> show count <> "/" <> show total
+
+        renderClearedCount :: Text -> Total -> Count -> IO ()
+        renderClearedCount x y z = cursorUpLine 1 *> clearLine *> renderCount x y z
+
+        renderStart :: Total -> IO ()
+        renderStart = flip (renderCount "Syncing") (Count 0)
+
+        renderSuccess :: Total -> IORef Count -> IO ()
+        renderSuccess total =
+          renderClearedCount "Syncing" total <=< flip atomicModifyIORef' (dup . (+ Count 1))
+
+        renderFin :: Total -> IORef Count -> IO ()
+        renderFin total = renderClearedCount "Synced" total <=< readIORef
+
+        renderFailed :: Failed -> IO ()
+        renderFailed (Failed []) = pure ()
+        renderFailed (Failed xs) =
+          let y = intercalate ", " (T.unpack . unpack <$> xs)
+           in putStrLn $ "The following feeds failed to sync: " <> y
+
+        pushFailed :: IORef Failed -> PodcastId -> IO ()
+        pushFailed x = modifyIORef' x . (<>) . Failed . pure
+
+        syncOne :: (PodcastId -> IO ()) -> IO () -> (PodcastId, Source) -> IO (Maybe (PodcastId, Feed))
+        syncOne onFailure onSuccess (fid, src) = do
+          res <- getPodcast . T.unpack . sourceUrl $ src
+          if isRight res then onSuccess else onFailure fid
+          pure $ (fid,) <$> fromRight Nothing res
 
 main :: IO ()
 main =
--- lib/Data/Tuple/Custom.hs
+++ lib/Data/Tuple/Custom.hs
@@ -1,4 +1,7 @@
-module Data.Tuple.Custom (fst3) where
+module Data.Tuple.Custom (dup, fst3) where
+
+dup :: a -> (a, a)
+dup x = (x, x)
 
 fst3 :: (a, b, c) -> a
 fst3 (x, _, _) = x
