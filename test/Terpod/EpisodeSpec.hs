03d272d5183d83d17bfd4d026715ace8f7115c83
Sam A. Horvath-Hunt <hello@samhh.com>
2021-06-27 19:22:29 +0100

    Fix tests

--- test/Terpod/EpisodeSpec.hs
+++ test/Terpod/EpisodeSpec.hs
@@ -1,5 +1,6 @@
 module Terpod.EpisodeSpec (spec) where
 
+import qualified Data.Text           as T
 import qualified Hedgehog.Gen        as Gen
 import qualified Hedgehog.Range      as Range
 import           Prelude
@@ -11,6 +12,6 @@ spec :: Spec
 spec = describe "Terpod.Episode" $ do
   describe "sanitise" $ do
     it "produces nothing other than lowercase chars, numbers, and hyphens" $ hedgehog $ do
-      str <- forAll $ Gen.list (Range.linear 1 99) Gen.ascii
-      all (flip elem $ ['a'..'z'] <> ['0'..'9'] <> ['-']) (sanitise str) === True
+      str <- forAll $ Gen.text (Range.linear 1 99) Gen.ascii
+      all (flip elem $ ['a'..'z'] <> ['0'..'9'] <> ['-']) (T.unpack . sanitise $ str) === True
 
