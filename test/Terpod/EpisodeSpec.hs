df4c35965011695f65ac630198529417c7e9c911
Sam A. Horvath-Hunt <hello@samhh.com>
2021-06-28 17:07:26 +0100

    Prefer assert in Hedgehog predicate test

--- test/Terpod/EpisodeSpec.hs
+++ test/Terpod/EpisodeSpec.hs
@@ -1,17 +1,18 @@
 module Terpod.EpisodeSpec (spec) where
 
 import qualified Data.Text           as T
+import           Hedgehog            (assert)
 import qualified Hedgehog.Gen        as Gen
 import qualified Hedgehog.Range      as Range
 import           Prelude
 import           Terpod.Episode      (sanitise)
 import           Test.Hspec
-import           Test.Hspec.Hedgehog (forAll, hedgehog, (===))
+import           Test.Hspec.Hedgehog (forAll, hedgehog)
 
 spec :: Spec
 spec = describe "Terpod.Episode" $ do
   describe "sanitise" $ do
     it "produces nothing other than lowercase chars, numbers, and hyphens" $ hedgehog $ do
       str <- forAll $ Gen.text (Range.linear 1 99) Gen.ascii
-      all (flip elem $ ['a'..'z'] <> ['0'..'9'] <> ['-']) (T.unpack . sanitise $ str) === True
+      assert $ all (flip elem $ ['a'..'z'] <> ['0'..'9'] <> ['-']) (T.unpack . sanitise $ str)
 
