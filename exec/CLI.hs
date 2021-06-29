dc93321df310440bbff1ed6c5716234f63b76712
Sam A. Horvath-Hunt <hello@samhh.com>
2021-06-29 15:49:08 +0100

    Add podcast ID completions
    Per optparse-applicative, these are accessible at for example
    --bash-completion-script, a hidden option. See:
    https://github.com/pcapriotti/optparse-applicative#bash-zsh-and-fish-completions

--- exec/CLI.hs
+++ exec/CLI.hs
@@ -1,9 +1,12 @@
 module CLI (parseOptions, Options (..), Command (..), ListOptions (..), Order (..)) where
 
-import           Data.Version        (showVersion)
-import qualified Options.Applicative as A
-import           Paths_terpod        (version)
-import           Terpod.Podcast      (PodcastId (PodcastId))
+import           Control.Newtype.Generics (unpack)
+import qualified Data.Text                as T
+import           Data.Version             (showVersion)
+import qualified Options.Applicative      as A
+import           Paths_terpod             (version)
+import           Terpod.Cache             (getPodcastIds)
+import           Terpod.Podcast           (PodcastId (PodcastId))
 
 newtype Options = Options
   { command :: Command
@@ -29,37 +32,35 @@ data Command
   | Download PodcastId Int
   deriving (Show)
 
+pidCompleter :: A.Completer
+pidCompleter = A.mkCompleter f
+  where f x = filter (x `isPrefixOf`) . fmap (T.unpack . unpack) <$> getPodcastIds
+
 syncParser :: A.Parser Command
 syncParser = pure Sync
 
 listParser :: A.Parser Command
-listParser =
-  fmap List $
-    ListOptions
-      <$> (fmap PodcastId <$> A.optional (A.argument A.str (A.metavar "PODCAST-ID")))
-      <*> A.optional (A.option A.auto (A.long "limit" <> A.short 'n' <> A.metavar "LIMIT" <> A.help "Limit the number of items output"))
-      <*> A.optional (A.option A.auto (A.long "offset" <> A.metavar "OFFSET" <> A.help "Offset items (prior to limiting)"))
-      <*> A.flag Newest Oldest (A.long "oldest" <> A.help "Sort by oldest")
+listParser = fmap List $ ListOptions
+  <$> (fmap PodcastId <$> A.optional (A.argument A.str (A.metavar "PODCAST-ID" <> A.completer pidCompleter)))
+  <*> A.optional (A.option A.auto (A.long "limit" <> A.short 'n' <> A.metavar "LIMIT" <> A.help "Limit the number of items output"))
+  <*> A.optional (A.option A.auto (A.long "offset" <> A.metavar "OFFSET" <> A.help "Offset items (prior to limiting)"))
+  <*> A.flag Newest Oldest (A.long "oldest" <> A.help "Sort by oldest")
 
 downloadParser :: A.Parser Command
-downloadParser =
-  Download
-    <$> A.argument A.str (A.metavar "PODCAST-ID")
-    <*> A.argument A.auto (A.metavar "INDEX")
+downloadParser = Download
+  <$> A.argument A.str (A.metavar "PODCAST-ID" <> A.completer pidCompleter)
+  <*> A.argument A.auto (A.metavar "INDEX")
 
 commandParser :: A.Parser Command
-commandParser =
-  A.subparser
-    ( A.command "sync" (A.info (A.helper <*> syncParser) $ A.progDesc "Sync podcast feeds")
-        <> A.command "list" (A.info (A.helper <*> listParser) $ A.progDesc "List the latest episodes of all podcasts")
-        <> A.command "download" (A.info (A.helper <*> downloadParser) $ A.progDesc "Download an episode by ID")
-    )
+commandParser = A.subparser $
+     A.command "sync" (A.info (A.helper <*> syncParser) $ A.progDesc "Sync podcast feeds")
+  <> A.command "list" (A.info (A.helper <*> listParser) $ A.progDesc "List the latest episodes of all podcasts")
+  <> A.command "download" (A.info (A.helper <*> downloadParser) $ A.progDesc "Download an episode by ID")
 
 parser :: A.ParserInfo Options
-parser =
-  A.info
-    (A.helper <*> v <*> (Options <$> commandParser))
-    (A.fullDesc <> A.progDesc d)
+parser = A.info
+  (A.helper <*> v <*> (Options <$> commandParser))
+  (A.fullDesc <> A.progDesc d)
   where d = "Manage podcasts from the command-line."
         v = A.infoOption (showVersion version) (A.short 'v' <> A.long "version" <> A.help "Output version")
 
