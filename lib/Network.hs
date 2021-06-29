ebd204e665d9e7f16291a947d8654ad004f21b31
Sam A. Horvath-Hunt <hello@samhh.com>
2021-06-29 16:42:38 +0100

    Try to get file ext from res header

--- lib/Network.hs
+++ lib/Network.hs
@@ -1,10 +1,13 @@
-module Network (getContentLength) where
+module Network (getContentLength, getContentType) where
 
-import Byte (Bytes (Bytes))
+import           Byte                  (Bytes (Bytes))
 import qualified Data.ByteString.Char8 as BS
-import Data.Functor.Custom ((<$<))
-import Network.HTTP.Simple (Response, getResponseHeader)
+import           Data.Functor.Custom   ((<$<))
+import           Network.HTTP.Simple   (Response, getResponseHeader)
 
 getContentLength :: Response a -> Maybe Bytes
 getContentLength = Bytes <$< readMaybe <=< BS.unpack <$< viaNonEmpty head . getResponseHeader "Content-Length"
+
+getContentType :: Response a -> Maybe ByteString
+getContentType = viaNonEmpty head . getResponseHeader "Content-Type"
 
--- lib/Terpod/Episode.hs
+++ lib/Terpod/Episode.hs
@@ -8,12 +8,16 @@ import           Control.Newtype.Generics (Newtype, unpack)
 import qualified Data.ByteString          as BS
 import           Data.Char                (isAlphaNum, toLower)
 import           Data.Conduit             (await)
+import qualified Data.Map                 as M
+import           Data.Set.NonEmpty        (NESet)
+import qualified Data.Set.NonEmpty        as NES
 import qualified Data.Text                as T
 import           Data.Time.Calendar       (Day)
-import           Network                  (getContentLength)
+import           Network                  (getContentLength, getContentType)
 import           Network.HTTP.Conduit     (http, newManager, responseBody,
                                            tlsManagerSettings)
 import           Network.HTTP.Simple      (parseRequest)
+import           Network.Mime             (Extension, MimeType, defaultMimeMap)
 import           System.Directory         (createDirectoryIfMissing)
 import           System.FilePath.Posix    (takeExtension, (</>))
 import           Terpod.Config            (DownloadPath, expandTilde,
@@ -59,14 +63,18 @@ downloadEpisode onProgress base pid ep = do
   let url = T.unpack $ episodeUrl ep
   req <- parseRequest url
   dir <- (</> T.unpack (unpack pid)) <$> expandTilde (unDownloadPath base)
-  let path = dir </> (T.unpack . sanitise . title $ ep) <> takeExtension url
   createDirectoryIfMissing True dir
   man <- newManager tlsManagerSettings
   runResourceT $ do
     res <- http req man
+    let mext = do
+          mime <- getContentType res
+          T.unpack . ("." <>) . pickExt mime <$> M.lookup mime extMap
+    let ext = fromMaybe (stripQs . takeExtension $ url) mext
+    let path = dir </> (T.unpack . sanitise . title $ ep) <> ext
     let total = getContentLength res
     sealConduitT (responseBody res) $$+- callbackProgress total 0 .| sinkFile path
-  pure path
+    pure path
     where callbackProgress :: Maybe Bytes -> Int -> ConduitT ByteString ByteString (ResourceT IO) ()
           callbackProgress total curr = await >>= foldMap inspect
             where inspect data' = do
@@ -74,3 +82,17 @@ downloadEpisode onProgress base pid ep = do
                     liftIO . onProgress . (, total) . Bytes . toInteger $ curr'
                     yield data'
                     callbackProgress total curr'
+
+          stripQs = takeWhile (/= '?')
+
+-- | Selects the preferred extension from a non-empty set. Has some bespoke
+-- rules for common mimetypes with multiple possible extensions, such as MP3,
+-- in which case the output is not guaranteed to be present in the input set.
+pickExt :: MimeType -> NESet Extension -> Extension
+pickExt "audio/mpeg" _ = "mp3"
+pickExt _ xs           = NES.findMin xs
+
+extMap :: Map MimeType (NESet Extension)
+extMap = invert defaultMimeMap
+  where invert :: (Ord k, Ord v) => Map k v -> Map v (NESet k)
+        invert = M.foldlWithKey (\acc k v -> M.insertWith NES.union v (NES.singleton k) acc) M.empty
